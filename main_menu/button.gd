c36fe0a917c71ba8f27798f8f57edbab90898faf
Lydia Symchych <94412624+lydia-symchych@users.noreply.github.com>
2025-01-26 15:08:27 -0600

    SFX for title menu
    Added SFX to each item in the game menu. Also added an audio player for the main menu at the end of the inspector - waiting for music to be done before adding that in

--- main_menu/button.gd
+++ main_menu/button.gd
@@ -1,9 +1,13 @@
 extends TextureButton
 	
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 func _on_mouse_entered() -> void:
 	animation_player.play("new_animation")
+	audio_stream_player_2d.play()
+	
 	
 func _on_mouse_exited() -> void:
 	animation_player.pause()
